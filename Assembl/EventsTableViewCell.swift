4b1c9c55cf054f7f5b968d3fc5e53b43e8fc7684
spwing <spwing@mxschool.edu>
2017-05-17 13:35:15 -0400

    adding events table view cell code

--- Assembl/EventsTableViewCell.swift
+++ Assembl/EventsTableViewCell.swift
@@ -10,14 +10,18 @@ import UIKit
 
 class EventsTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var infoLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
 
